60834fa3dc515083f0447c5cbefde3bbe620bbaf
AngryFender <Teabiscuitperson@gmail.com>
2025-02-06 23:51:49 +0000

    using inet_ntop to get ip addresses

--- src/plainconnection.h
+++ src/plainconnection.h
@@ -6,22 +6,49 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 void TryConnection()
 {
     int status;
     struct addrinfo hints;
     struct addrinfo *servinfo;
+    struct addrinfo *p;
+    char ipstr[INET6_ADDRSTRLEN];
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+    // hints.ai_flags = AI_PASSIVE;
 
-    if(( status = getaddrinfo(NULL,"8080",&hints, &servinfo)) != 0){
+    if(( status = getaddrinfo("www.google.com","8080",&hints, &servinfo)) != 0){
         std::cout<<"getaddrinfo error:"<< gai_strerror(status)<<"\n";
+        return;
     }
+
+    for(p = servinfo; p != NULL; p=p->ai_next)
+    {
+        void *addr;
+        char *ipver;
+
+        if(p->ai_family == AF_INET)
+        {
+            struct sockaddr_in *ipv4 = reinterpret_cast<struct sockaddr_in*>(p->ai_addr);
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        }
+        else
+        {
+            struct sockaddr_in6 *ipv6 = reinterpret_cast<struct sockaddr_in6*>(p->ai_addr);
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        std::cout<<ipver<<": "<<ipstr<<"\n" ;
+    }
+
 
     freeaddrinfo(servinfo);
 }
