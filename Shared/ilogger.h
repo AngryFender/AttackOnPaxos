9b88acd2557f1b098a50a21f9b4d458b0c4d8f14
AngryFender <Teabiscuitperson@gmail.com>
2025-03-07 23:51:27 +0000

    Adding mechanism to logs error to the system

---
+++ Shared/ilogger.h
@@ -0,0 +1,26 @@
+#ifndef ILOGGER_H
+#define ILOGGER_H
+
+enum LogType
+{
+    DEBUG = 0,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+};
+
+class ILogger
+{
+public:
+    virtual ~ILogger() = default;
+    virtual ILogger& operator << (const char* message) = 0;
+    virtual ILogger& setLogLevel(const LogType& type) = 0;
+    virtual void registerDebugCallback(LogDebug logDebug) = 0;
+    virtual void registerInfoCallback(LogInfo logInfo) = 0;
+    virtual void registerWarningCallback(LogWarning logWarning) = 0;
+    virtual void registerErrorCallback(LogError logError) = 0;
+    virtual void registerFatalCallback(LogFatal logFatal) = 0;
+};
+
+#endif //ILOGGER_H
