ed7ef476c2ebb361d94dda04f8bf72fea75faae1
R <Teabiscuitperson@gmail.com>
2025-03-10 22:09:00 +0000

    Updated logger.cpp to overload << operator

--- Shared/logger.cpp
+++ Shared/logger.cpp
@@ -6,15 +6,28 @@ Logger::~Logger()
 
 ILogger& Logger::operator<<(const char* message)
 {
+    std::lock_guard<std::mutex> lockGuard(_mutex);
+    switch(_logType)
+    {
+    case DEBUG: _debugCall(message);break;
+    case INFO: _infoCall(message); break;
+    case WARNING: _warningCall(message); break;
+    case ERROR: _errorCall(message); break;
+    case FATAL: _fatalCall(message); break;
+    }
 
-}
+    return *_logger;
+} 
 
 ILogger& Logger::setLogLevel(const LogType& type)
 {
+    std::lock_guard<std::mutex> lockGuard(_mutex);
+    _logType = type;
 }
 
 void Logger::registerDebugCallback(LogType& type, LogCallBack logCall)
 {
+    std::lock_guard<std::mutex> lockGuard(_mutex);
     switch (type)
     {
     case DEBUG: _debugCall = logCall; break;
