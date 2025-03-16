5c5a471cf07cdb59d07cc42305ed47256e3b1f7d
AngryFender <teabiscuitperson@gmail.com>
2025-03-16 23:42:44 +0000

    Changed the signature to register callback using std::function

--- Shared/ilogger.h
+++ Shared/ilogger.h
@@ -1,6 +1,8 @@
 #ifndef ILOGGER_H
 #define ILOGGER_H
 
+#include <functional>
+#include <string>
 typedef void (*LogCallBack)(const char* message);
 
 enum LogType
@@ -18,7 +20,7 @@ public:
     virtual ~ILogger() = default;
     virtual ILogger& operator << (const char* message) = 0;
     virtual ILogger& setLogLevel(const LogType& type) = 0;
-    virtual void registerDebugCallback(LogType& type, LogCallBack logCall) = 0;
+    virtual bool registerCallback(LogType& type, std::function<void(const std::string& message)> LogCallBack);
 };
 
 #endif //ILOGGER_H
