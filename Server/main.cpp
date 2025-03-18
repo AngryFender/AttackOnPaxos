ed7ab0d980fed2f6d8aef9a7841330f870bc09c9
AngryFender <teabiscuitperson@gmail.com>
2025-03-18 23:58:04 +0000

    Starting to build init

--- Server/main.cpp
+++ Server/main.cpp
@@ -2,12 +2,24 @@
 
 #include "../Shared/asioconnection.h"
 #include "../Shared/plainconnection.h"
+#include "../Shared/ilogger.h"
+#include "../Shared/logger.h"
 
+void init()
+{
+
+}
 
 
 int main()
 {
     std::cout << "Attack on Paxos!" << std::endl;
+    init();
+    Logger::getInstance();
+    Logger::getInstance().registerCallback([](LogType& type, std::string& message){
+        std::cout<<message;
+    });
+
     //SetupSimpleServer();
     SetupAsioServerAsync();
     return 0;
