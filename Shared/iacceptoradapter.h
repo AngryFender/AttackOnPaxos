8bc0edd472f9faa9e958ce7baaffb0240a0c0ce2
AngryFender <Teabiscuitperson@gmail.com>
2025-03-23 22:42:02 +0000

    Added interfaces for Socket adapter and Acceptor adapter

---
+++ Shared/iacceptoradapter.h
@@ -0,0 +1,8 @@
+//
+// Created by angry on 23/03/25.
+//
+
+#ifndef IACCEPTORADAPTER_H
+#define IACCEPTORADAPTER_H
+
+#endif //IACCEPTORADAPTER_H
---
+++ Shared/isocketadapter.h
@@ -0,0 +1,8 @@
+//
+// Created by angry on 23/03/25.
+//
+
+#ifndef ISOCKETADAPTER_H
+#define ISOCKETADAPTER_H
+
+#endif //ISOCKETADAPTER_H
