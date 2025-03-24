0a1574ab33ed88bff687deabaef38abdf6187723
AngryFender <Teabiscuitperson@gmail.com>
2025-03-24 20:22:05 +0000

    Added missing update from the last commit

--- Shared/iacceptoradapter.h
+++ Shared/iacceptoradapter.h
@@ -1,8 +1,16 @@
-//
-// Created by angry on 23/03/25.
-//
-
 #ifndef IACCEPTORADAPTER_H
 #define IACCEPTORADAPTER_H
+#include <functional>
+#include <memory>
+#include <boost/system/detail/error_code.hpp>
+#include "isocketadapter.h"
+
+using boost::system::error_code;
+class IAcceptorAdapter
+{
+public:
+    virtual ~IAcceptorAdapter() = default;
+    virtual void async_accept(std::shared_ptr<ISocketAdapter> socket, std::function<void(error_code& error)>) = 0;
+};
 
 #endif //IACCEPTORADAPTER_H
