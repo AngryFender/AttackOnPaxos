b2be4bc1421926dc725f0847d81dba2890425e92
AngryFender <Teabiscuitperson@gmail.com>
2025-04-11 23:55:41 +0100

    Modified the constructor of the ConnectionManager class to support resolver and socket connection in the future

--- Shared/connectionmanager.cpp
+++ Shared/connectionmanager.cpp
@@ -2,19 +2,31 @@
 
 void ConnectionManager::AddConnection(std::shared_ptr<ISocketAdapter> socketAdapter)
 {
-    std::lock_guard lock(_mutex);
+    std::unique_lock lock(_mutex);
     _connections[socketAdapter->getSocket().remote_endpoint().address().to_string()] = socketAdapter;
+
+    // tcp::resolver::query query(ipAddress,_port,boost::asio::ip::resolver_base::numeric_service);
+    // tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+    // tcp::resolver::iterator end;
+    //
+    // tcp::socket socket(io_context);
+    // boost::system::error_code error = boost::asio::error::host_not_found;
+    // while (error && endpoint_iterator != end)
+    // {
+    //     socket.close();
+    //     socket.connect(*endpoint_iterator++, error);
+    // }
 }
 
 void ConnectionManager::RemoveConnection(const std::string address)
 {
-    std::lock_guard lock(_mutex);
+    std::unique_lock lock(_mutex);
     _connections.erase(address);
 }
 
 bool ConnectionManager::GetConnection(const std::string address, std::shared_ptr<ISocketAdapter>& socketAdapter) const
 {
-    std::lock_guard lock(_mutex);
+    std::shared_mutex lock(_mutex);
     const auto it = _connections.find(address);
     if(it != _connections.end())
     {
@@ -26,6 +38,6 @@ bool ConnectionManager::GetConnection(const std::string address, std::shared_ptr
 
 std::map<std::string, std::shared_ptr<ISocketAdapter>> ConnectionManager::GetConnections() const
 {
-    std::lock_guard lock(_mutex);
+    std::shared_mutex lock(_mutex);
     return _connections;
 }
--- Shared/connectionmanager.h
+++ Shared/connectionmanager.h
@@ -1,18 +1,26 @@
 #ifndef CONNECTIONMANAGER_H
 #define CONNECTIONMANAGER_H
+#include <shared_mutex>
 #include "iconnectionmanager.h"
 
 class ConnectionManager: public IConnectionManager
 {
 public:
     ~ConnectionManager() override = default;
+    explicit ConnectionManager(boost::asio::io_context& io_context,const std::string& port): _resolver(io_context), _port(port)
+    {
+
+    };
+
     void AddConnection(std::shared_ptr<ISocketAdapter> socketAdapter) override;
     void RemoveConnection(const std::string address) override;
-    bool ConnectionManager::GetConnection(const std::string address, std::shared_ptr<ISocketAdapter>& socketAdapter) const override;
+    bool GetConnection(const std::string address, std::shared_ptr<ISocketAdapter>& socketAdapter) const override;
     std::map<std::string, std::shared_ptr<ISocketAdapter>> GetConnections() const override;
 
 private:
-    std::mutex _mutex;
+    tcp::resolver _resolver;
+    const std::string _port;
+    mutable std::shared_mutex _mutex;
     std::map<std::string, std::shared_ptr<ISocketAdapter>> _connections;
 };
 
