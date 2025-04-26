7f4788abccd4ef2d23ef2fbc135793ef9051095b
AngryFender <Teabiscuitperson@gmail.com>
2025-04-26 23:47:55 +0100

    Added inteface for implementing Paxos and Raft algorithms

--- Shared/concord.h
+++ Shared/concord.h
@@ -1,14 +1,17 @@
 #ifndef CONCORD_H
 #define CONCORD_H
 #include "iconnectionmanager.h"
+#include "istrategy.h"
 
 class Concord {
 public:
-    explicit Concord(IConnectionManager& connection_manager): _manager(connection_manager)
+    explicit Concord(IConnectionManager& connection_manager, IStrategy& strategy):
+        _manager(connection_manager), _strategy(strategy)
     {
-
     }
+
     IConnectionManager& _manager;
+    IStrategy& _strategy;
 };
 
 #endif //CONCORD_H
---
+++ Shared/istrategy.h
@@ -0,0 +1,11 @@
+
+#ifndef ISTRATEGY_H
+#define ISTRATEGY_H
+
+class IStrategy
+{
+public:
+    virtual ~IStrategy() = default;
+};
+
+#endif //ISTRATEGY_H
