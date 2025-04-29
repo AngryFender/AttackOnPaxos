5bc338be670d330c9b5936e64bba8b4064f0ada0
R <Teabiscuitperson@gmail.com>
2025-04-29 23:06:48 +0100

    Updated contents of prepare packet to send to acceptors

--- Shared/paxos.cpp
+++ Shared/paxos.cpp
@@ -3,6 +3,17 @@
 void Paxos::SendPrepareID(const uint64_t id)
 {
     //send prepare id to all acceptor nodes
+    Proposal p;
+    p.id = htonl(id);
+    p.type = State::Prepare;
+    p.length = htonl(sizeof(p.id) + sizeof (p.type));
+
+    for(const auto& connection_pair: _manager->GetConnections())
+    {
+        const auto& socket = connection_pair.second;
+        
+    }
+    
 }
 
 void Paxos::SendAcceptRequest(const uint64_t id)
