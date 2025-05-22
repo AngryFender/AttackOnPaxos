fed339789ac665b3994d2b93cdd3c6e0f7408e9b
AngryFender <Teabiscuitperson@gmail.com>
2025-05-22 23:47:31 +0100

    Updated the node to keep sending the same message over and over again to check the receiving node

--- Node/main.cpp
+++ Node/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "../Shared/plainconnection.h"
 #include "../Shared/connectionmanager.h"
+#include "../Shared/acceptoradapter.h"
+#include "../Shared/paxos.h"
 
 #define PORTNO 3491
 
@@ -8,19 +10,29 @@ void init_logs()
 {
     Logger::getInstance().registerCallback(LogType::DEBUG, [](const std::string& message)
     {
-        std::cout<<message;
+        std::cout << message;
     });
 
     Logger::getInstance().registerCallback(LogType::INFO, [](const std::string& message)
     {
-        std::cout<<message;
+        std::cout << message;
     });
 
     Logger::getInstance().registerCallback(LogType::ERROR, [](const std::string& message)
     {
-        std::cerr<<message;
+        std::cerr << message;
     });
     Log(INFO) << "Attack on Paxos!" << "\n";
+}
+
+void restart_timer(boost::asio::deadline_timer& timer, Paxos& pax)
+{
+    pax.SendPrepare(69);
+    timer.expires_from_now(boost::posix_time::seconds(2));
+    timer.async_wait([&pax, &timer](const boost::system::error_code&)
+    {
+        restart_timer(timer, pax);
+    });
 }
 
 void init_tcp_server()
@@ -30,6 +42,20 @@ void init_tcp_server()
         Log(INFO) << "Starting Node" << "\n";
         boost::asio::io_context io_context;
         ConnectionManager connectionManager(io_context, PORTNO, std::make_shared<AcceptorAdapter>(io_context, PORTNO));
+
+        const std::string address = "127.0.0.1";
+        boost::asio::ip::basic_endpoint<tcp> end_point(boost::asio::ip::address::from_string(address), 3490);
+        std::shared_ptr<ISocketAdapter> socket = std::make_shared<SocketAdapter>(io_context);
+
+        connectionManager.AddConnection(address, end_point, socket);
+        Paxos pax(connectionManager);
+        boost::asio::deadline_timer timer(io_context, boost::posix_time::seconds(2));
+
+        timer.async_wait([&pax, &timer](const boost::system::error_code&)
+        {
+            // pax.SendPrepare(69);
+            restart_timer(timer,pax);
+        });
 
         io_context.run();
     }
@@ -41,9 +67,9 @@ void init_tcp_server()
 
 int main(int argc, char* argv[])
 {
-    if(argc < 2)
+    if (argc < 2)
     {
-        std::cout<< "ip-address missing as param\n";
+        std::cout << "ip-address missing as param\n";
         return -1;
     }
     init_logs();
