19471219c1bf5eb0b7f647d511a470b7e4cfdaf1
AngryFender <Teabiscuitperson@gmail.com>
2025-07-22 23:29:47 +0100

    Added implementation of the Heartbeat class

--- Shared/heartbeat.cpp
+++ Shared/heartbeat.cpp
@@ -2,15 +2,20 @@
 
 void Heartbeat::Start()
 {
-
+    _timer.expires_after(std::chrono::milliseconds(_milliseconds));
+    _timer.async_wait([this](const boost::system::error_code& code)
+    {
+        this->_handler();
+        this->Start();
+    });
 }
 
 void Heartbeat::Stop()
 {
-
+    _timer.cancel();
 }
 
 void Heartbeat::SetHandler(std::function<void()> handler)
 {
-
+    _handler = handler;
 }
--- Shared/heartbeat.h
+++ Shared/heartbeat.h
@@ -1,16 +1,26 @@
 #ifndef HEARTBEAT_H
 #define HEARTBEAT_H
 
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include "iheartbeat.h"
 
 class Heartbeat: public IHeartbeat
 {
-    std::function<void()> _handler;
 public:
-    ~Heartbeat() = default;
+    explicit Heartbeat(boost::asio::io_context &io_context, const int milliseconds): _io_context(io_context), _timer(io_context), _milliseconds(milliseconds)
+    {
+    }
+
+    ~Heartbeat() override = default;
     void Start() override;
     void Stop() override;
     void SetHandler(std::function<void()> handler) override;
+private:
+    boost::asio::io_context& _io_context;
+    boost::asio::steady_timer _timer;
+    int _milliseconds;
+    std::function<void()> _handler;
 };
 
 
